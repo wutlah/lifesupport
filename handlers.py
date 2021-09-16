11bffb65e53eb952f5cfbfb7923a940016177982
eve <everett@unrestricted.host>
2021-09-16 20:12:15 +0800

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -56,7 +56,7 @@ def forward_to_user(update, context):
         user_id = update.message.reply_to_message.forward_from.id
     elif REPLY_TO_THIS_MESSAGE in update.message.reply_to_message.text:
         try:
-            user_id = int(update.message.reply_to_message.text.split('From'user_info.username'\n')[0])
+            user_id = int(update.message.reply_to_message.text.split('\n')[0])
         except ValueError:
             user_id = None
     if user_id:

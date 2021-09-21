b673696f1a7836855e126b7a47d1be88f18a4d94
eve <everett@unrestricted.host>
2021-09-21 22:38:41 +0800

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -12,40 +12,51 @@ def start(update, context):
     context.bot.send_message(
         chat_id=TELEGRAM_SUPPORT_CHAT_ID,
         text=f"""
-📞 Connected {user_info}.
+📞 @{update.message.from_user.username}/{update.message.from_user.id} started the chat.
         """,
     )
 
+def end(update, context):
+    update.message.reply_text("Thank you and stay safe.\nFeel free to chat with us if you have any other questions.")
+
+    user_info = update.message.from_user.to_dict()
+
+    context.bot.send_message(
+        chat_id=TELEGRAM_SUPPORT_CHAT_ID,
+        text=f"""
+📞 @{update.message.from_user.username} ended the chat.
+         """,
+    )
 
 def forward_to_chat(update, context):
     """{ 
         'message_id': 5, 
         'date': 1605106546, 
-        'chat': {'id': 49820636, 'type': 'private', 'username': 'danokhlopkov', 'first_name': 'Daniil', 'last_name': 'Okhlopkov'}, 
+        'chat': {'id': -, 'type': '-', 'username': '-', 'first_name': '-', 'last_name': '-'}, 
         'text': 'TEST QOO', 'entities': [], 'caption_entities': [], 'photo': [], 'new_chat_members': [], 'new_chat_photo': [], 'delete_chat_photo': False, 'group_chat_created': False, 'supergroup_chat_created': False, 'channel_chat_created': False, 
-        'from': {'id': 49820636, 'first_name': 'Daniil', 'is_bot': False, 'last_name': 'Okhlopkov', 'username': 'danokhlopkov', 'language_code': 'en'}
+        'from': {'id': -, 'first_name': '-', 'is_bot': False, 'last_name': 'Okhlopkov', 'username': '-', 'language_code': 'en'}
     }"""
     forwarded = update.message.forward(chat_id=TELEGRAM_SUPPORT_CHAT_ID)
     if not forwarded.forward_from:
         context.bot.send_message(
             chat_id=TELEGRAM_SUPPORT_CHAT_ID,
             reply_to_message_id=forwarded.message_id,
-            text=f'{update.message.from_user.id}\n{REPLY_TO_THIS_MESSAGE}'
+            text=f'From: @{update.message.from_user.username}\n{REPLY_TO_THIS_MESSAGE}'
         )
 
 
 def forward_to_user(update, context):
     """{
         'message_id': 10, 'date': 1605106662, 
-        'chat': {'id': -484179205, 'type': 'group', 'title': '☎️ SUPPORT CHAT', 'all_members_are_administrators': True}, 
+        'chat': {'id': -, 'type': 'group', 'title': '☎️ SUPPORT CHAT', 'all_members_are_administrators': True}, 
         'reply_to_message': {
             'message_id': 9, 'date': 1605106659, 
-            'chat': {'id': -484179205, 'type': 'group', 'title': '☎️ SUPPORT CHAT', 'all_members_are_administrators': True}, 
-            'forward_from': {'id': 49820636, 'first_name': 'Daniil', 'is_bot': False, 'last_name': 'Okhlopkov', 'danokhlopkov': 'okhlopkov', 'language_code': 'en'}, 
+            'chat': {'id': 000, 'type': 'group', 'title': '☎️ SUPPORT CHAT', 'all_members_are_administrators': True}, 
+            'forward_from': {'id': 0, 'first_name': 'Daniil', 'is_bot': False, 'last_name': 'Okhlopkov', 'danokhlopkov': 'okhlopkov', 'language_code': 'en'}, 
             'forward_date': 1605106658, 
             'text': 'g', 'entities': [], 'caption_entities': [], 'photo': [], 'new_chat_members': [], 'new_chat_photo': [], 
             'delete_chat_photo': False, 'group_chat_created': False, 'supergroup_chat_created': False, 'channel_chat_created': False, 
-            'from': {'id': 1440913096, 'first_name': 'SUPPORT', 'is_bot': True, 'username': 'lolkek'}
+            'from': {'id': 0, 'first_name': 'SUPPORT', 'is_bot': True, 'username': 'lolkek'}
         }, 
         'text': 'ggg', 'entities': [], 'caption_entities': [], 'photo': [], 'new_chat_members': [], 'new_chat_photo': [], 'delete_chat_photo': False, 
         'group_chat_created': False, 'supergroup_chat_created': False, 'channel_chat_created': False, 
@@ -74,6 +85,7 @@ def forward_to_user(update, context):
 
 def setup_dispatcher(dp):
     dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('end', end))
     dp.add_handler(MessageHandler(Filters.chat_type.private, forward_to_chat))
     dp.add_handler(MessageHandler(Filters.chat(TELEGRAM_SUPPORT_CHAT_ID) & Filters.reply, forward_to_user))
     return dp
